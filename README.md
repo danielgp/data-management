240912e8a202c1335fde4307c4c0ad430899b03f
Daniel Popiniuc <danielpopiniuc@gmail.com>
2020-04-05 21:10:17 +0300

    initial beta release

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# data-management
-Data manipulation package
+# Data manipulation package
+
+
+## What is this repository for?
+
+Facilitate data management (aka manipulation).
+
+## Who do I talk to?
+
+Repository owner is: [Daniel Popiniuc](mailto:daniel.popiniuc@honeywell.com)
+
+
+## Installation
+
+Installation can be completed in few steps as follows:
+* Ensure you have git available to your system:
+```
+    $ git --version
+```
+> If you get an error depending on your system you need to install it.
+>> For Windows you can do so from [Git for Windows](https://github.com/git-for-windows/git/releases/);
+* Download this project from Github:
+```
+    $ git clone https://github.com/danielgp/tableau-hyper-management
+```
+* Create a Python Virtual Environment using following command executed from project root folder:
+```
+    $ python -m venv virtual_environment/
+```
+* Upgrade pip (PIP is a package manager for Python packages) and SetupTools using following command executed from newly created virtual environment and Scripts sub-folder:
+```
+    $ python -m pip install --upgrade pip --user
+    $ pip install --upgrade setuptools --user
+```
+* Install project prerequisites using following command executed from project root folder:
+```
+    $ python setup.py install
+```
+
+
+## Usage
+
+### Merging CSV files based on a matching pattern within a given folder to a single CSV file
+```
+    $ python <local_path_of_this_package>/sources/merger.py --input-directory <input_directory_full_path> --input-file-pattern "*.csv" --csv-field-separator ","|";"|"|" --output-file <full_path_and_file_base_name_to_generated_file>(.hyper) (--output-log-file <full_path_and_file_name_to_log_running_details>)
+```
+- conventions used:
+    - (content_within_round_parenthesis) = optional
+    - <content_within_html_tags> = variables to be replaced with user values relevant strings
+    - single vertical pipeline = separator for alternative options
+
+## Change Log / Releases detailed
+
+see [CHANGE_LOG.md](CHANGE_LOG.md)
+
+## Code of conduct
+
+Use [CODE_OF_CONDUCT.md](.github/CODE_OF_CONDUCT.md)
+
+## Features to request template
+
+Use [feature_request.md](.github/ISSUE_TEMPLATE/feature_request.md)
+
+## Bug report template
+
+Use [bug_report.md](.github/ISSUE_TEMPLATE/bug_report.md)
+
+## Code quality analysis
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/danielgp/data-management/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/danielgp/data-management/?branch=master)
+
+## Build Status
+[![Build Status](https://scrutinizer-ci.com/g/danielgp/data-management/badges/build.png?b=master)](https://scrutinizer-ci.com/g/danielgp/data-management/build-status/master)
+
---
+++ sources/merger.py
@@ -0,0 +1,50 @@
+# package to facilitate operating system operations
+import os
+# useful methods to measure time performance by small pieces of code
+from codetiming import Timer
+# Custom classes specific to this package
+from data_management.BasicNeeds import BasicNeeds
+from data_management.CommandLineArgumentsManagement import CommandLineArgumentsManagement
+from data_management.LoggingNeeds import LoggingNeeds
+from data_management.DataManipulator import DataManipulator
+
+# get current script name
+current_script_name = os.path.basename(__file__).replace('.py', '')
+
+# main execution logic
+if __name__ == '__main__':
+    # instantiate Basic Needs class
+    c_bn = BasicNeeds()
+    # load application configuration (inputs are defined into a json file)
+    c_bn.fn_load_configuration()
+    # instantiate Command Line Arguments class
+    c_clam = CommandLineArgumentsManagement()
+    parameters_in = c_clam.parse_arguments(c_bn.cfg_dtls['input_options']['merger'])
+    # checking inputs, if anything is invalid an exit(1) will take place
+    c_bn.fn_check_inputs(parameters_in, current_script_name)
+    # instantiate Logger class
+    c_ln = LoggingNeeds()
+    # initiate logger
+    c_ln.initiate_logger(parameters_in.output_log_file, 'dm_merger')
+    # define global timer to use
+    t = Timer('dm_merger', text='Time spent is {seconds} ', logger=c_ln.logger.debug)
+    # reflect title and input parameters given values in the log
+    c_clam.listing_parameter_values(c_ln.logger, t, 'Data Merger',
+                                    c_bn.cfg_dtls['input_options']['merger'], parameters_in)
+    # instantiate Basic Needs class
+    c_dm = DataManipulator()
+    # loading from a specific folder all files matching a given pattern into a file list
+    relevant_files_list = c_dm.fn_build_relevant_file_list(c_ln.logger, t,
+                                                           parameters_in.input_directory,
+                                                           parameters_in.input_file_pattern)
+    # loading from a specific folder all files matching a given pattern into a data frame
+    working_data_frame = c_dm.fn_load_file_list_to_data_frame(c_ln.logger, t,
+                                                              relevant_files_list,
+                                                              parameters_in.csv_field_separator)
+    # store data frame into single CSV file
+    c_dm.fn_store_data_frame_to_file(working_data_frame, parameters_in.output_file,
+                                     parameters_in.csv_field_separator)
+    # store statistics about output file
+    c_bn.fn_store_file_statistics(c_ln.logger, t, parameters_in.output_log_file, 'Generated')
+    # just final message
+    c_bn.fn_final_message(c_ln.logger, parameters_in.output_log_file, t.timers.total('dm_merger'))
