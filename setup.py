48b97fd041e9869245efc8ef70a850bfa2e2622e
Daniel Popiniuc <danielpopiniuc@gmail.com>
2020-04-13 23:01:28 +0300

    version bumping reflected in setup script and change-log

--- setup.py
+++ setup.py
@@ -4,18 +4,20 @@ setup - ensures proper package setup
 This file is ensuring proper package setup is performed to ensure all prerequisites are satisfied 
 and correct execution is possible
 """
+# package to handle files/folders and related metadata/operations
+import os.path
 # facilitate dependencies management
 from setuptools import setup, find_packages
 
-with open('README.md', 'r') as fh:
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as fh:
     long_description_readme = fh.read()
 
 this_package_website = 'https://github.com/danielgp/data-management'
 
 setup(
-    author = 'Daniel Popiniuc',
-    author_email = 'danielpopiniuc@gmail.com',
-    classifiers = [
+    author='Daniel Popiniuc',
+    author_email='danielpopiniuc@gmail.com',
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
@@ -28,31 +30,32 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering :: Information Analysis'
     ],
-    description = 'Wrapper to ease data management into Tableau Hyper format from CSV files',
-    include_package_data = True,
-    install_requires = [
+    description='Wrapper to ease data management into Tableau Hyper format from CSV files',
+    include_package_data=True,
+    install_requires=[
         'codetiming>=1.1,<=1.2',
         'pandas>=0.25.3,<=1.0.3'
     ],
-    keywords = [
+    keywords=[
         'csv'
     ],
-    license = 'LGPL3',
-    long_description = long_description_readme,
-    long_description_content_type = 'text/markdown',
-    name = 'data-management',
-    packages = find_packages('data_management'),
-    package_data = {
+    license='LGPL3',
+    long_description=long_description_readme,
+    long_description_content_type='text/markdown',
+    name='data-management',
+    packages=find_packages('data_management'),
+    package_data={
         'data_management': [
             '*.json'
         ]
     },
-    project_urls = {
+    project_urls={
         'Documentation': this_package_website + '/blob/master/README.md',
-        'Issue Tracker': this_package_website + '/issues?q=is%3Aissue+is%3Aopen+sort%3Aupdated-desc',
+        'Issue Tracker': this_package_website +
+                         '/issues?q=is%3Aissue+is%3Aopen+sort%3Aupdated-desc',
         'Source Code': this_package_website
     },
-    python_requires = '>=3.6',
-    url = this_package_website + '/releases',  # project home page, if any
-    version = '0.3.3',
+    python_requires='>=3.6',
+    url=this_package_website + '/releases',  # project home page, if any
+    version='0.3.4',
 )
